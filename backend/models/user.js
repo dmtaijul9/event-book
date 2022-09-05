76f2cc97eafa2abbca187ae9cf634aceae4ccbbd
Taijul Islam <taijulislam.st9@gmail.com>
2022-09-05 22:03:25 +0600

    course video upto 7

---
+++ backend/models/user.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdEvents: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Event",
+    },
+  ],
+});
+
+module.exports = model("User", userSchema);
